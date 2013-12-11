252bdb73c1947c14ca2faae1eeda7b9b255a4ed2
Sean Bakko <sbakko@Seans-MacBook-Pro.local>
2013-12-10 20:15:54 -0500

    Initial Commit

---
+++ My First Project/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  My First Project
+//
+//  Created by Sean Bakko on 12/10/13.
+//  Copyright (c) 2013 Sean Bakko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
